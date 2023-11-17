42d3f76df2feba70627d10d29210a34d3f7425c5
Paulo Cardozo <paulo.cardozo@orbtall.com>
2023-11-17 07:33:29 -0300

    Initial commit

---
+++ src/Orbtall/DbBladeCompiler/DbBladeCompilerEngine.php
@@ -0,0 +1,16 @@
+<?php namespace Orbtall\DbBladeCompiler;
+
+use Illuminate\View\Engines\CompilerEngine;
+
+class DbBladeCompilerEngine extends CompilerEngine
+{
+    /**
+     * DbBladeCompilerEngine constructor.
+     *
+     * @param DbBladeCompiler $bladeCompiler
+     */
+    public function __construct(DbBladeCompiler $bladeCompiler)
+    {
+        parent::__construct($bladeCompiler);
+    }
+}
